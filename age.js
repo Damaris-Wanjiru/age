639948eb97e71af34c9b143b42c0de8ae078f2b5
Damaris-Wanjiru <damarshih@gmail.com>
2024-04-01 15:44:32 +0300

    calculated my sister's age in javascrip

---
+++ age.js
@@ -0,0 +1,10 @@
+// define my current age
+const myAge = 44;
+
+// age difference
+const ageDifference = 2;
+
+// my sis age
+const sisAge = myAge - ageDifference;
+
+console.log("my sis is " + sisAge +"years old.");
